30ddb35aaed5930f2e8e70a04bceca9699b47042
TheDoctor205 <101677207+TheDoctor205@users.noreply.github.com>
2022-08-05 22:19:26 -0300

    css iniciado

--- backend/cadastrar_produto.php
+++ backend/cadastrar_produto.php
@@ -7,14 +7,26 @@ try{
     $produto = $_POST['produto'];
     $marca = $_POST['marca'];
     $categoria = $_POST['categoria'];
-    $data = $_POST['data'];
-    $preco = $_POST['preco'];
+    $validade = $_POST['validade'];
+    $valor = $_POST['valor'];
 
-    $sql = "INSERT INTO tb_produtos (produto,marca,categoria,`data`,preco) value ('$produto','$marca','$categoria','$data','$preco',)";
-    echo "Cadastrado com sucesso";
+    $sql = "INSERT INTO tb_produtos (produto,marca,categoria,validade,valor) value ('$produto','$marca','$categoria','$validade','$valor')";
+
+    $comando = $con->prepare($sql);
+
+    // executa o comando cpm a query no banco de dados
+    $comando->execute();
+
+    // exibe msg de sucesso ao inserir
+    echo "Cadastro realizado com sucesso";
+
+    // forçar fim da conexão
+    $con = null;
+    
 
 }catch(PDOException $erro){
     echo $erro->getMessage();
+    die();
 }
 
 ?>
