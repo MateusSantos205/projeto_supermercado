c55f296ca2ca813381c8f04d7b8cbd6c577fd1f9
TheDoctor205 <101677207+TheDoctor205@users.noreply.github.com>
2022-08-10 22:19:34 -0300

    deletar produtos adicionado

---
+++ admin/gerenciar_produtos.php
@@ -0,0 +1,79 @@
+<?php
+
+    include '../backend/conexao.php';
+
+    try{
+        $sql = "SELECT *FROM tb_produtos";
+
+        $comando = $con->prepare($sql);
+
+        $comando->execute();
+
+        $dados = $comando->fetchALL(PDO::FETCH_ASSOC);
+
+        // echo "<pre>";
+        // var_dump($dados);
+        // echo "</pre>";
+
+    }catch(PDOException $erro){
+        echo $erro->getMessage();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style_table.css">
+    <title>Gerenciar Produtos</title>
+</head>
+<body>
+
+    <div id="container" >
+
+    <h3>Gerenciar Produtos</h3>
+
+    <div id="tabela" >
+
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Produto</th>
+                <th>Marca</th>
+                <th>Categoria</th>
+                <th>Data de Validade</th>
+                <th>Preço</th>
+            </tr>
+            <!--  -->
+            <?php
+                foreach ($dados as $produtos):
+            ?>
+            <!--  -->
+            <tr>
+                <td><?php echo $produtos['id']; ?></td>
+                <td><?php echo $produtos['produto']; ?></td>
+                <td><?php echo $produtos['marca']; ?></td>
+                <td><?php echo $produtos['categoria']; ?></td>
+                <td><?php echo $produtos['validade']; ?></td>
+                <td>R$<?php echo $produtos['valor']; ?></td>
+                <td>Alterar</td>
+                <td><a href="../backend/_deletar_produtos.php?id=<?php echo $produtos['id']; ?>">Deletar</a></td>
+            </tr>
+
+            <?php
+                endforeach;
+            ?>
+
+        </table>
+
+    </div>
+
+
+    </div>
+
+    
+</body>
+</html>
---
+++ backend/_deletar_produtos.php
@@ -0,0 +1,20 @@
+<?php
+
+    include 'conexao.php';
+
+    try{
+        $id = $_GET['id'];
+
+        $sql = "DELETE FROM tb_produtos WHERE id=$id";
+
+        $comando = $con->prepare($sql);
+
+        $comando ->execute();
+
+        header('location:../admin/gerenciar_produtos.php');
+
+    }catch(PDOException $erro){
+        echo $erro->getMessage();
+    }
+
+?>
